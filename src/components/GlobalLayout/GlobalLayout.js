03e28a837942f5e852a98ca72ddb91c127aa2c44
Myron <dubtwee@gmail.com>
2020-10-04 21:50:45 -0500

    creating a global layout for the application to persist throughout every view

---
+++ src/components/GlobalLayout/GlobalLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './GlobalLayout.module.css';
+
+function GlobalLayout(props) {
+  const HeaderContent = props.header ? props.header : () => <h1>Header</h1>;
+  const FooterContent = props.footer ? props.footer : () => <p>Footer</p>;
+
+  return (
+    <div className={styles.scaffold}>
+      <div className={styles['scaffold-hd']}>
+        <HeaderContent />
+      </div>
+      <div className={styles['scaffold-bd']}>{props.children}</div>
+      <div>
+        <FooterContent />
+      </div>
+    </div>
+  );
+}
+
+export default GlobalLayout;
