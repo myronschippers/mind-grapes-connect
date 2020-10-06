4472745935cb5c174893bab82f7d5dae4f216b5c
Myron <dubtwee@gmail.com>
2020-10-05 21:34:28 -0500

    making it so that all pages render based on what is in nav.config.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
 import GlobalLayout from '../GlobalLayout/GlobalLayout';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
+// MODULES
+import navConfig from '../MainNav/nav.config';
+
 function App() {
   return (
-    <GlobalLayout header={Header} footer={Footer}>
-      Body Content Here
-    </GlobalLayout>
+    <Router>
+      <GlobalLayout header={Header} footer={Footer}>
+        Body Content Here
+        {navConfig.map((pgConfig) => {
+          return (
+            <Route exact path={pgConfig.path} component={pgConfig.component} />
+          );
+        })}
+      </GlobalLayout>
+    </Router>
   );
 }
 
