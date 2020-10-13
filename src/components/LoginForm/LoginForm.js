8c5dadff08b9b27f13e0239ec5f445ee0fe98b79
Myron <dubtwee@gmail.com>
2020-10-12 23:13:08 -0500

    updating login page layout and form to fully leverage Material-UI

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,20 +1,45 @@
 import React, { useState } from 'react';
 
 // MATERIAL-UI COMPONENTS
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Collapse, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import { Alert } from '@material-ui/lab';
+
+// MATERIAL-UI CUSTOM STYLING
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 
 function LoginForm(props) {
-  const [loginError, setLoginError] = useState(null);
+  const classes = useStyles();
+  const [loginError, setLoginError] = useState({ show: false, message: null });
   const [loginInfo, setLoginInfo] = useState({ email: '', password: '' });
 
-  function handleLoginSubmit(event) {
+  const handleLoginSubmit = function (event) {
     event.preventDefault();
     console.log('Login With:', loginInfo);
     // TODO - only display error when there is a problem logging in
-    setLoginError(
-      'There was something wrong with your email or password. Please try again.'
-    );
-  }
+    setLoginError({
+      show: true,
+      message:
+        'There was something wrong with your email or password. Please try again.',
+    });
+  };
+
+  const closeError = function () {
+    setLoginError({
+      ...loginError,
+      show: false,
+    });
+  };
 
   const handleFieldChange = (dataKey) => (event) => {
     setLoginInfo({
@@ -24,20 +49,43 @@ function LoginForm(props) {
   };
 
   return (
-    <form onSubmit={handleLoginSubmit}>
+    <form className={classes.root} onSubmit={handleLoginSubmit}>
+      <Collapse in={loginError.show}>
+        <Alert
+          severity="error"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={closeError}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          {loginError.message}
+        </Alert>
+      </Collapse>
       <TextField
         id="userEmail"
-        type="text"
+        type="email"
         onChange={handleFieldChange('email')}
-        label="Email:"
+        label="Email"
+        variant="outlined"
+        required
       />
       <TextField
         id="userPassword"
         type="password"
         onChange={handleFieldChange('password')}
-        label="Password:"
+        label="Password"
+        variant="outlined"
+        required
       />
-      <Button type="submit">Login</Button>
+      <div>
+        <Button type="submit">CONTINUE</Button>
+      </div>
     </form>
   );
 }
--- src/views/LoginPg/LoginPg.js
+++ src/views/LoginPg/LoginPg.js
@@ -1,21 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // MATERIAL-UI COMPONENTS
-import { Typography } from '@material-ui/core';
+import { Typography, Box, Paper } from '@material-ui/core';
 
 // CUSTOM COMPONENTS
 import LoginForm from '../../components/LoginForm/LoginForm';
 
+// MATERIAL-UI CUSTOM STYLING
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxSizing: 'border-box',
+    width: '400px',
+    maxWidth: '85%',
+    margin: '0 auto',
+  },
+  loginHd: {
+    paddingBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    textAlign: 'center',
+    position: 'relative',
+    '&::before': {
+      content: '""',
+      width: '80%',
+      height: '1px',
+      backgroundColor: theme.palette.divider,
+      position: 'absolute',
+      bottom: '0',
+      left: '50%',
+      transform: `translate(-50%, 0)`,
+    },
+  },
+}));
+
 function LoginPg(props) {
-  const [loginError, setLoginError] = useState(0);
+  const classes = useStyles();
 
   return (
-    <div>
-      <Typography component="h2" variant="h4">
-        Login
-      </Typography>
-      <LoginForm />
-    </div>
+    <Box component="div" className={classes.root}>
+      <Paper>
+        <Box component="div" p={3}>
+          <Typography component="h2" variant="h4" className={classes.loginHd}>
+            Login
+          </Typography>
+          <LoginForm />
+        </Box>
+      </Paper>
+    </Box>
   );
 }
 
