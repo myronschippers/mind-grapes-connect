307b0b611ef48109e551bb32bd5fa25322de3d72
Myron <dubtwee@gmail.com>
2020-10-14 13:35:21 -0500

    creating a reusable component for the layout used on the login page

---
+++ src/components/PgLayoutCenterBox/PgLayoutCenterBox.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+// MATERIAL-UI COMPONENTS
+import { Typography, Box, Paper } from '@material-ui/core';
+
+// MATERIAL-UI CUSTOM STYLING
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxSizing: 'border-box',
+    width: '400px',
+    maxWidth: '85%',
+    margin: '0 auto',
+  },
+  pageHd: {
+    paddingBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    textAlign: 'center',
+    position: 'relative',
+    '&::before': {
+      content: '""',
+      width: '80%',
+      height: '1px',
+      backgroundColor: theme.palette.divider,
+      position: 'absolute',
+      bottom: '0',
+      left: '50%',
+      transform: `translate(-50%, 0)`,
+    },
+  },
+}));
+
+function PgLayoutCenterBox(props) {
+  const classes = useStyles();
+
+  return (
+    <Box component="div" className={classes.root}>
+      <Paper>
+        <Box component="div" p={3}>
+          <Typography component="h2" variant="h4" className={classes.pageHd}>
+            {props.title}
+          </Typography>
+          {props.children}
+        </Box>
+      </Paper>
+    </Box>
+  );
+}
+
+export default PgLayoutCenterBox;
--- src/views/LoginPg/LoginPg.js
+++ src/views/LoginPg/LoginPg.js
@@ -6,49 +6,14 @@ import { Typography, Box, Paper } from '@material-ui/core';
 // CUSTOM COMPONENTS
 import LoginForm from '../../components/LoginForm/LoginForm';
 import PageWrapper from '../../components/PageWrapper/PageWrapper';
-
-// MATERIAL-UI CUSTOM STYLING
-import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles((theme) => ({
-  root: {
-    boxSizing: 'border-box',
-    width: '400px',
-    maxWidth: '85%',
-    margin: '0 auto',
-  },
-  loginHd: {
-    paddingBottom: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-    textAlign: 'center',
-    position: 'relative',
-    '&::before': {
-      content: '""',
-      width: '80%',
-      height: '1px',
-      backgroundColor: theme.palette.divider,
-      position: 'absolute',
-      bottom: '0',
-      left: '50%',
-      transform: `translate(-50%, 0)`,
-    },
-  },
-}));
+import PgLayoutCenterBox from '../../components/PgLayoutCenterBox/PgLayoutCenterBox';
 
 function LoginPg(props) {
-  const classes = useStyles();
-
   return (
     <PageWrapper>
-      <Box component="div" className={classes.root}>
-        <Paper>
-          <Box component="div" p={3}>
-            <Typography component="h2" variant="h4" className={classes.loginHd}>
-              Login
-            </Typography>
-            <LoginForm />
-          </Box>
-        </Paper>
-      </Box>
+      <PgLayoutCenterBox title="Login">
+        <LoginForm />
+      </PgLayoutCenterBox>
     </PageWrapper>
   );
 }
