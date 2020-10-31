977d07ed29beafa25dc7f0f53eb7ff42fdc73ce8
Myron <dubtwee@gmail.com>
2020-10-30 23:27:03 -0500

    altering the database name to connect to the propper local database

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -10,7 +10,7 @@ const url = require('url');
 let config = {
   host: 'localhost', // Server hosting the postgres database
   port: 5432, // env var: PGPORT
-  database: '_auth-table-rewrite', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+  database: '_mind-vineyard', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
