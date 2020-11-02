47585168dcc30e5b788f5f5144256b170587033e
Myron <dubtwee@gmail.com>
2020-11-01 23:05:44 -0600

    altering pool to be a typescript file

--- server/modules/pool.ts
+++ server/modules/pool.ts
@@ -6,11 +6,13 @@
 const pg = require('pg');
 const url = require('url');
 
+const ENV_CONFIG: object = process.env != null ? process.env : {};
+
 // default configuration for local development
-let config = {
-  host: 'localhost', // Server hosting the postgres database
-  port: 5432, // env var: PGPORT
-  database: '_mind-vineyard', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+let config: object = {
+  host: ENV_CONFIG.DB_HOST ? ENV_CONFIG.DB_HOST : 'localhost', // Server hosting the postgres database
+  port: ENV_CONFIG.DB_PORT ? ENV_CONFIG.DB_PORT : 5432, // env var: PGPORT
+  database: ENV_CONFIG.DB_NAME ? ENV_CONFIG.DB_NAME : '_mind-vineyard', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
@@ -35,7 +37,7 @@ if (process.env.DATABASE_URL) {
 }
 
 // this creates the pool that will be shared by all other modules
-const pool = new pg.Pool(config);
+const pool: pg.Pool = new pg.Pool(config);
 
 // the pool with emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
